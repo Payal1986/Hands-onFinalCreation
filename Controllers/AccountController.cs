5f1dac6468663be21aeff99d9f2d1f78b46f18ab
Payal1986 <EDUCBA@educba@1234>
2024-03-11 17:35:56 +0530

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Hands_onFinalCreation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hands_onFinalCreation.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        //post: /Account/Login
+        [HttpPost]
+        public IActionResult Login(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.UserName == "administrator" && model.Password == "admin123")
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid Username or Password");
+                    return View(model);
+                }
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+    }
+}
